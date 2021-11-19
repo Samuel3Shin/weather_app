71f3cea102150ceaaec1c01dd107b77fc9eb0246
Samuel3Shin <samuel3.shin@gmail.com>
2021-11-19 13:26:57 -0800

    detail acitonbar customized

--- app/src/main/java/com/example/weather/DetailsActivity.kt
+++ app/src/main/java/com/example/weather/DetailsActivity.kt
@@ -1,12 +1,49 @@
 package com.example.weather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class
 DetailsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        // calling the action bar
+        var actionBar = supportActionBar
+        actionBar?.title = "Los Angeles, California"
+
+        // showing the back button in action bar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Back button clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+                onBackPressed()
+            }
+
+        }
+        return true
+    }
+
+    // Twitter button
+    // create an action bar button
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        menuInflater.inflate(R.menu.mymenu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
